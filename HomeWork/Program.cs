dab4c667421fc72515eb1c5d1945c6b58ac7ea5e
mak-p <mak-p@DESKTOP-PG1IP70>
2023-01-30 21:42:28 +0300

    Task 2 done

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+// Task 1
+/* 
+Write("Введите число в диапозоне от 1 до 100: ");
+int num = Convert.ToInt32(ReadLine());
+
+for(int i=1;i<100;i++)
+{
+if (num > 100 || num < 1)
+    {
+        WriteLine("Вы вышли за приделы диапозона,поробуйте снова");
+        break;
+    }
+else if (num % 3 == 0 && num % 5 == 0) 
+    {
+        WriteLine("Fizz Buzz");
+        break;
+    }
+else if (num % 3 == 0)
+    {
+        WriteLine("Fizz");
+        break;
+    }
+else if (num % 5 == 0)
+    {
+        WriteLine("Buzz");
+        break;
+    }
+else
+    {
+        WriteLine(num);
+        break;
+    }
+}
+*/
+
+
+WriteLine("Ввудите два числа ");
+Write("Введите значение: ");
+double num1=Convert.ToDouble(ReadLine());
+Write("Введите процент: ");
+double num2=Convert.ToDouble(ReadLine());
+
+double result;
+result = num1 * num2 / 100;
+WriteLine($"Результат: {result}");
