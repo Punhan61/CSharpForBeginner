35661ff5175d99d82cf7177a46b432165baecbf9
mak-p <mak-p@DESKTOP-PG1IP70>
2023-01-31 14:31:55 +0300

    Task 3 done

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -1,4 +1,6 @@
-﻿using static System.Console;
+﻿using System.Security.Principal;
+using System.Threading.Tasks.Dataflow;
+using static System.Console;
 
 // Task 1
 /* 
@@ -35,8 +37,9 @@ else
 }
 */
 
-
-WriteLine("Ввудите два числа ");
+// Task 2
+/*
+WriteLine("Введите два числа ");
 Write("Введите значение: ");
 double num1=Convert.ToDouble(ReadLine());
 Write("Введите процент: ");
@@ -45,3 +48,30 @@ double num2=Convert.ToDouble(ReadLine());
 double result;
 result = num1 * num2 / 100;
 WriteLine($"Результат: {result}");
+*/
+
+// Task 3
+//Пользователь вводит с клавиатуры четыре цифры.
+//Необходимо создать число, содержащее эти цифры.  
+/*
+Write("Введите первую цифру:");
+int num1 = Convert.ToInt32(ReadLine());
+Write("Введите вторую цифру:");
+int num2 = Convert.ToInt32(ReadLine());
+Write("Введите третью цифру:");
+int num3 = Convert.ToInt32(ReadLine());
+Write("Введите четвертую цифру: ");
+int num4 = Convert.ToInt32(ReadLine());
+WriteLine($"Созданное число: {num1}{num2}{num3}{num4}");
+*/
+
+// Task 4
+//Пользователь вводит шестизначное число. После чего
+//пользователь вводит номера разрядов для обмена цифр.
+Write("Введите шестизначное число: ");
+int num1=Convert.ToInt32(ReadLine());
+WriteLine("Введите номера разрядов для обмена цифр: ");
+Write("Первый разряд: ");
+int num2=Convert.ToInt32(ReadLine());
+Write("Второй разряд: ");
+int num3=Convert.ToInt32(ReadLine());
